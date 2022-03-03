18af8d965837e727c8948b5f3a67d8ba7fbfc04f
Mark Grand <mgrand@markgrand.com>
2022-03-03 18:47:55 -0500

    process_command_line passes unit test

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate anyhow;
 extern crate log;
 
 use anyhow::{bail, Context, Result};
-use log::{error,info};
+use log::{error, info};
 use std::env;
 use std::fs::File;
 use std::io::{BufReader, Read};
@@ -11,7 +11,7 @@ use std::process::exit;
 fn main() {
     env_logger::init();
     info!("Starting");
-    if let Err(error) = do_it() {
+    if let Err(error) = run() {
         eprintln!("{}", error);
         error!("Exiting due to error: {}", error);
         exit(1);
@@ -19,18 +19,67 @@ fn main() {
     info!("normal completion");
 }
 
-fn do_it() -> Result<()> {
-    let reader = process_command_line()?;
+fn run() -> Result<()> {
+    let _reader = process_command_line(env::args().collect())?;
     Ok(())
 }
 
 // Return a reader for the input.
-fn process_command_line() -> Result<Box<dyn Read>> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() == 1 {
-        let file = File::open(&args[0]).with_context(|| format!("Error opening {}", args[0]))?;
+fn process_command_line(args: Vec<String>) -> Result<Box<dyn Read>> {
+    if args.len() == 2 {
+        let file_name = &args[1];
+        let file = File::open(file_name).with_context(|| format!("Error opening {}", args[0]))?;
+        info!("Reading from {}", file_name);
         Ok(Box::new(BufReader::new(file)))
     } else {
         bail!("Expect exactly on file name on the command line");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::process_command_line;
+    use std::fs::{remove_file, File};
+    use std::io::Write;
+
+    #[test]
+    fn process_command_line_wrong_arg_count() {
+        if let Ok(_) = process_command_line(vec!["exe".to_string()]) {
+            panic!("No error for zero args")
+        }
+        if let Ok(_) = process_command_line(vec![
+            "exe".to_string(),
+            "asdf".to_string(),
+            "extra".to_string(),
+        ]) {
+            panic!("No error for two args")
+        }
+    }
+
+    #[test]
+    fn process_command_line_nonexistent_file() {
+        if let Ok(_) = process_command_line(vec!["exe".to_string(), "bogus".to_string()]) {
+            panic!("No error for zero args")
+        }
+    }
+
+    const TRANSACTION_FILE_CONTENT: &str = r##"type, client, tx, amount
+deposit, 1, 1, 1.0
+deposit, 2, 2, 2.0
+deposit, 1, 3, 2.0
+withdrawal, 1, 4, 1.5
+withdrawal, 2, 5, 3.0"##;
+
+    #[test]
+    fn process_command_line_good_file() -> Result<()> {
+        let file_name = "cli_test_file";
+        {
+            let mut file = File::create(file_name)?;
+            file.write_all(TRANSACTION_FILE_CONTENT.as_bytes())?;
+        }
+        let _file = process_command_line(vec!["exe".to_string(), file_name.to_string()])?;
+        remove_file(file_name)?;
+        Ok(())
     }
 }
